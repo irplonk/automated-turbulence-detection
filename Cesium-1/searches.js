a8b8b3338efe8315b6536e17bb84625d7108b2ce
William Samuelson <rwilliamjr1@gmail.com>
2017-11-29 13:15:57 -0500

    Fix airport labels only appearing for one result

--- Cesium-1/searches.js
+++ Cesium-1/searches.js
@@ -1,62 +1,55 @@
 
 LabelCollectionGeocoder.prototype.geocode = function (input) {
-  var searchtext = input;
+  viewer.entities.removeAll();
   var searchlist = [];
 
-  var gcLC = tax_labels;
-  var len = gcLC.length;
-  for (var i = 0; i < len; ++i) {
-    var l = gcLC.get(i);
-      if ( l.text.toLowerCase().indexOf( searchtext.toLowerCase() ) > -1 ) {
-          searchlist.push(l);
+  for (var i = 0; i < airport_labels.length; ++i) {
+    var airport = airport_labels.get(i);
+      if (airport.text.toLowerCase().indexOf(input.toLowerCase()) > -1) {
+          searchlist.push(airport);
       }
   }
 
   return Cesium.loadText("").then(function (results) {
-          var bboxDegrees;
-          return searchlist.map(function (resultObject) {
-              var lonlat = Cesium.Ellipsoid.WGS84.cartesianToCartographic(resultObject.position);
+    return searchlist.map(function (resultObject) {
+        var lonlat = Cesium.Ellipsoid.WGS84.cartesianToCartographic(resultObject.position);
 
-              var heightmin = 10000;
-              var heightmax = 10000;
+        var heightmin = 10000;
+        var heightmax = 10000;
 
-              if (resultObject.distanceDisplayCondition.near) heightmin = resultObject.distanceDisplayCondition.near;
-              if (resultObject.distanceDisplayCondition.far) heightmax = resultObject.distanceDisplayCondition.far;
-              var horizdeg = Math.sqrt(.5*6371000*(heightmax+heightmin)/2)/111000;
-              var nwlat = lonlat.latitude + Math.PI/180*horizdeg/2; if (nwlat > Math.PI/2) nwlat=(nwlat/Math.PI/2) % 1 * Math.PI/2;
-              var nwlon = lonlat.longitude + Math.PI/360*horizdeg; if (nwlon > Math.PI) nwlon=(nwlon/Math.PI - 1) % 1 * Math.PI;
-              var swlat = lonlat.latitude - Math.PI/180*horizdeg/2; if (swlat < -Math.PI/2) swlat=(swlat/Math.PI/2) % 1 * Math.PI/2;
-              var swlon = lonlat.longitude - Math.PI/360*horizdeg; if (swlon < -Math.PI) swlon=(swlon/Math.PI + 1) % 1 * Math.PI;
-              var carto = [
-                new Cesium.Cartographic(swlon, swlat, heightmin),
-                new Cesium.Cartographic(nwlon, nwlat, heightmax)
-              ];
+        if (resultObject.distanceDisplayCondition.near) heightmin = resultObject.distanceDisplayCondition.near;
+        if (resultObject.distanceDisplayCondition.far) heightmax = resultObject.distanceDisplayCondition.far;
+        var horizdeg = Math.sqrt(.5*6371000*(heightmax+heightmin)/2)/111000;
+        var nwlat = lonlat.latitude + Math.PI/180*horizdeg/2; if (nwlat > Math.PI/2) nwlat=(nwlat/Math.PI/2) % 1 * Math.PI/2;
+        var nwlon = lonlat.longitude + Math.PI/360*horizdeg; if (nwlon > Math.PI) nwlon=(nwlon/Math.PI - 1) % 1 * Math.PI;
+        var swlat = lonlat.latitude - Math.PI/180*horizdeg/2; if (swlat < -Math.PI/2) swlat=(swlat/Math.PI/2) % 1 * Math.PI/2;
+        var swlon = lonlat.longitude - Math.PI/360*horizdeg; if (swlon < -Math.PI) swlon=(swlon/Math.PI + 1) % 1 * Math.PI;
+        var carto = [
+          new Cesium.Cartographic(swlon, swlat, heightmin),
+          new Cesium.Cartographic(nwlon, nwlat, heightmax)
+        ];
+        var recto = Cesium.Rectangle.fromCartographicArray(carto);
 
-              var recto = Cesium.Rectangle.fromCartographicArray(carto);
-              var returnObject =  {
-                  displayName: resultObject.text,
-                  destination: recto
-              };
+        var returnObject =  {
+            displayName: resultObject.text,
+            destination: recto
+        };
 
-              searchedLocation = returnObject.displayName;
+        var searchResult = findObjectByKey(airports, "name", returnObject.displayName);
 
-              var searchResult = findObjectByKey(airports, "name", searchedLocation);
-
-              viewer.entities.removeAll();
-
-              viewer.entities.add({
-                  name: searchResult.name,
-                  position: Cesium.Cartesian3.fromDegrees(parseFloat(searchResult.lon), parseFloat(searchResult.lat)),
-                  point: {
-                    pixelSize: 5,
-                    color: Cesium.Color.RED,
-                    outlineColor: Cesium.Color.WHITE,
-                    outlineWidth: 2
-                  }
-              })
-              return returnObject;
-          });
-      });
+        viewer.entities.add({
+            name: searchResult.name,
+            position: Cesium.Cartesian3.fromDegrees(parseFloat(searchResult.lon), parseFloat(searchResult.lat)),
+            point: {
+              pixelSize: 5,
+              color: Cesium.Color.RED,
+              outlineColor: Cesium.Color.WHITE,
+              outlineWidth: 2
+            }
+        })
+        return returnObject;
+    });
+  });
 };
 
 function findObjectByKey(array, key, value) {
@@ -68,15 +61,13 @@ function findObjectByKey(array, key, value) {
   return null;
 }
 
-var tax_labels = viewer.scene.primitives.add(new Cesium.LabelCollection());
-
 viewer.dataSources.add(Cesium.CzmlDataSource.load(airports));
 
-var tax_labels = viewer.scene.primitives.add(new Cesium.LabelCollection() );
+var airport_labels = viewer.scene.primitives.add(new Cesium.LabelCollection());
 
 for (var i = 1; i < airports.length; i++) {
   if((airports[i].lat != null) && (airports[i].lon != null) && (airports[i].name != null) && (airports[i].iso == "US")) {
-      tax_labels.add({
+      airport_labels.add({
       position : Cesium.Cartesian3.fromDegrees(parseFloat(airports[i].lon), parseFloat(airports[i].lat)),
       text : airports[i].name,
       font : '15.75px sans-serif',
