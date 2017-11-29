d9b6505bcec30d3e6dfe57907ec01784d9debfea
William Samuelson <rwilliamjr1@gmail.com>
2017-11-29 12:45:38 -0500

    Formatted searches.js

--- Cesium-1/searches.js
+++ Cesium-1/searches.js
@@ -1,14 +1,5 @@
 
-var searchedLocation ="";
-
-var searchArr = [];
-var tempArr = [];
-
-console.log(searchedLocation);
-var searchResult;
-
 LabelCollectionGeocoder.prototype.geocode = function (input) {
-
   var searchtext = input;
   var searchlist = [];
 
@@ -21,12 +12,9 @@ LabelCollectionGeocoder.prototype.geocode = function (input) {
       }
   }
 
-  return Cesium.loadText("")
-      .then(function (results) {
+  return Cesium.loadText("").then(function (results) {
           var bboxDegrees;
           return searchlist.map(function (resultObject) {
-
-
               var lonlat = Cesium.Ellipsoid.WGS84.cartesianToCartographic(resultObject.position);
 
               var heightmin = 10000;
@@ -35,19 +23,14 @@ LabelCollectionGeocoder.prototype.geocode = function (input) {
               if (resultObject.distanceDisplayCondition.near) heightmin = resultObject.distanceDisplayCondition.near;
               if (resultObject.distanceDisplayCondition.far) heightmax = resultObject.distanceDisplayCondition.far;
               var horizdeg = Math.sqrt(.5*6371000*(heightmax+heightmin)/2)/111000;
-
-
-
               var nwlat = lonlat.latitude + Math.PI/180*horizdeg/2; if (nwlat > Math.PI/2) nwlat=(nwlat/Math.PI/2) % 1 * Math.PI/2;
               var nwlon = lonlat.longitude + Math.PI/360*horizdeg; if (nwlon > Math.PI) nwlon=(nwlon/Math.PI - 1) % 1 * Math.PI;
               var swlat = lonlat.latitude - Math.PI/180*horizdeg/2; if (swlat < -Math.PI/2) swlat=(swlat/Math.PI/2) % 1 * Math.PI/2;
               var swlon = lonlat.longitude - Math.PI/360*horizdeg; if (swlon < -Math.PI) swlon=(swlon/Math.PI + 1) % 1 * Math.PI;
               var carto = [
-                      new Cesium.Cartographic(swlon, swlat, heightmin),
-                      new Cesium.Cartographic(nwlon, nwlat, heightmax)
-                          ];
-             console.log(carto);
-
+                new Cesium.Cartographic(swlon, swlat, heightmin),
+                new Cesium.Cartographic(nwlon, nwlat, heightmax)
+              ];
 
               var recto = Cesium.Rectangle.fromCartographicArray(carto);
               var returnObject =  {
@@ -55,19 +38,9 @@ LabelCollectionGeocoder.prototype.geocode = function (input) {
                   destination: recto
               };
 
-              console.log(recto);
-
-
               searchedLocation = returnObject.displayName;
 
-
-              searchResult = searchForPlace(searchedLocation);
-
-
-              console.log(searchResult);
-              tempArr[0] = searchResult;
-
-              console.log(tempArr);
+              var searchResult = findObjectByKey(airports, "name", searchedLocation);
 
               viewer.entities.removeAll();
 
@@ -81,61 +54,33 @@ LabelCollectionGeocoder.prototype.geocode = function (input) {
                     outlineWidth: 2
                   }
               })
-
-
               return returnObject;
-
-
-
-
           });
-          console.log(returnObject);
-
-
       });
-
-
 };
-
-
-      console.log(searchResult);
 
 function findObjectByKey(array, key, value) {
   for (var i = 0; i < array.length; i++) {
-      if (ariports[i][key] === value) {
+      if (array[i][key] === value) {
           return array[i];
       }
   }
   return null;
 }
 
-
-var tax_labels = viewer.scene.primitives.add(new Cesium.LabelCollection() );
-
-
-function searchForPlace (searchedLocation) {
-var searchResult = null;
-  for (var i = 0; i < airports.length; i++) {
-    if(airports[i].name == searchedLocation) {
-      var searchResult = airports[i];
-      return searchResult;
-    }
-
-}
-return searchResult;
-}
-
+var tax_labels = viewer.scene.primitives.add(new Cesium.LabelCollection());
 
 viewer.dataSources.add(Cesium.CzmlDataSource.load(airports));
 
 var tax_labels = viewer.scene.primitives.add(new Cesium.LabelCollection() );
 
 for (var i = 1; i < airports.length; i++) {
-if((airports[i].lat != null) && (airports[i].lon != null) && (airports[i].name != null) && (airports[i].iso == "US") ) {
- tax_labels.add( {position : Cesium.Cartesian3.fromDegrees(parseFloat(airports[i].lon), parseFloat(airports[i].lat)),
-  text : airports[i].name,
-  font : '15.75px sans-serif',
-  distanceDisplayCondition : new Cesium.DistanceDisplayCondition(10000,20000)
-  });
-}
+  if((airports[i].lat != null) && (airports[i].lon != null) && (airports[i].name != null) && (airports[i].iso == "US")) {
+      tax_labels.add({
+      position : Cesium.Cartesian3.fromDegrees(parseFloat(airports[i].lon), parseFloat(airports[i].lat)),
+      text : airports[i].name,
+      font : '15.75px sans-serif',
+      distanceDisplayCondition : new Cesium.DistanceDisplayCondition(10000,20000)
+    });
+  }
 }
