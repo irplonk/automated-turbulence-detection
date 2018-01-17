b8acfe81b023c632b50f8eec85da28a9b29adcf6
Isabella Plonk <irplonk@gatech.edu>
2018-01-17 16:16:03 -0500

    Began creating DB models

--- server/server/urls.py
+++ server/server/urls.py
@@ -16,5 +16,5 @@ Including another URLconf
 from django.conf.urls import include, url
 
 urlpatterns = [
-    url(r'^$', include('turb.urls')),
+    url(r'^', include('turb.urls')),
 ]
--- server/turb/models.py
+++ server/turb/models.py
@@ -5,5 +5,18 @@ from django.db import models
 
 # Create your models here.
 
+class Aircraft(models.Model):
+    aircraft_type = models.CharField(max_length=10)
+    weight = models.DecimalField(max_digits=20, decimal_places=2)
+
 class WeatherReport(models.Model):
-    time = models.
+    time = models.DateTimeField()
+    latitude = models.DecimalField(max_digits=9, decimal_places=6)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6)
+    aircraft_type = models.ForeignKey(Aircraft)
+    temperature = models.DecimalField(max_digits=5, decimal_places=2)
+    wind_vector = models.DecimalField(max_digits=5, decimal_places=2)
+    acceleration = models.DecimalField(max_digits=5, decimal_places=2)
+
+class Airport(models.Model):
+    airport_code = models.CharField(max_length=10)
