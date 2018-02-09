e222b05539d478e2809c6f729463871b9052ae78
William Samuelson <rwilliamjr1@gmail.com>
2018-02-09 12:02:15 -0500

    Fix aircraft_type model and import of Aircraft
    Add on_delete to aircraft_type foreign key and changed models Airport

--- server/turb/models.py
+++ server/turb/models.py
@@ -16,7 +16,7 @@ class WeatherReport(models.Model):
     time = models.DateTimeField()
     latitude = models.DecimalField(max_digits=9, decimal_places=6)
     longitude = models.DecimalField(max_digits=9, decimal_places=6)
-    aircraft_type = models.ForeignKey(Aircraft)
+    aircraft_type = models.ForeignKey(Aircraft, on_delete=models.CASCADE)
     temperature = models.DecimalField(max_digits=5, decimal_places=2)
     wind_vector = models.DecimalField(max_digits=5, decimal_places=2)
     acceleration = models.DecimalField(max_digits=5, decimal_places=2)
