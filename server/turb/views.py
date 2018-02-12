91a0fd64503130c8a14e77f41b5fa6fae31349e3
William Samuelson <rwilliamjr1@gmail.com>
2018-02-12 16:04:33 -0500

    Integrate current html and weather data with django
    Moved html and weather_data javascript into appropriate directories. Added proper htmlresponse for index.

--- server/turb/views.py
+++ server/turb/views.py
@@ -3,12 +3,8 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from django.http import HttpResponse
 from . import models
-import os
-
-PACKAGE_ROOT = os.path.dirname(os.path.abspath(__file__))
-PROJECT_ROOT = os.path.join(PACKAGE_ROOT, os.pardir, os.pardir)
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse(open(os.path.join(PROJECT_ROOT, 'Starter.html'), "r"))
+    return render(request, 'index.html', {})
