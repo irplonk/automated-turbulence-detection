b02219ef529c3ea6173146e9d6f0d39e8a05be62
Sean Lu <slu81@gatech.edu>
2018-02-12 11:21:45 -0500

    DB Models

--- server/turb/models.py
+++ server/turb/models.py
@@ -8,7 +8,7 @@ from django.db import connection
 
 
 class Aircraft(models.Model):
-    aircraft_type = models.CharField(max_length=10)
+    aircraft_type = models.CharField(max_length=10, primary_key=True)
     weight = models.DecimalField(max_digits=20, decimal_places=2)
 
 
@@ -16,11 +16,13 @@ class WeatherReport(models.Model):
     time = models.DateTimeField()
     latitude = models.DecimalField(max_digits=9, decimal_places=6)
     longitude = models.DecimalField(max_digits=9, decimal_places=6)
+    altitude = models.DecimalField(max_digits=9, decimal_places=6)
     aircraft_type = models.ForeignKey(Aircraft, on_delete=models.CASCADE)
-    temperature = models.DecimalField(max_digits=5, decimal_places=2)
-    wind_vector = models.DecimalField(max_digits=5, decimal_places=2)
-    acceleration = models.DecimalField(max_digits=5, decimal_places=2)
+    wind_x = models.DecimalField(max_digits=5, decimal_places=2)
+    wind_y = models.DecimalField(max_digits=5, decimal_places=2)
+    tke = models.DecimalField(max_digits=5, decimal_places=2)
 
 
 class Airport(models.Model):
-    airport_code = models.CharField(max_length=10)
+    airport_code = models.CharField(max_length=3, primary_key=True)
+    airport_name = models.TextField()
