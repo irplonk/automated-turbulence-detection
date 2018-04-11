8a12bd1bdae3e0d12292c0c5ee7943b5e7def252
Isabella Plonk <irplonk@gatech.edu>
2018-04-11 10:42:42 -0400

    Remove unnecessary files and update settings to include db changes

--- server/server/settings.py
+++ server/server/settings.py
@@ -84,11 +84,7 @@ WSGI_APPLICATION = 'server.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'HOST': env('DB_HOST'),
-        'USER': env('DB_USER'),
-        'PASSWORD': env('DB_PASSWORD'),
-        'PORT': env('DB_PORT')
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
     }
 }
 
