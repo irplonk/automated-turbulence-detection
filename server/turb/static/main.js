ef09e289d023b795d12045d0669f661dfec7b339
Patel, Dylan Avnish <dpatel91@gatech.edu>
2018-04-21 22:26:18 -0400

    toggle

--- server/turb/static/main.js
+++ server/turb/static/main.js
@@ -288,3 +288,18 @@ function zoomed() {
 function stopped() {
     if (d3.event.defaultPrevented) d3.event.stopPropagation();
 }
+
+
+// document.addEventListener('DOMContentLoaded', function () {
+//   var checkbox = document.querySelector('input[type="checkbox"]');
+
+//   checkbox.addEventListener('change', function () {
+//     if (checkbox.checked) {
+//       // do this
+//       console.log('Checked');
+//     } else {
+//       // do that
+//       console.log('Not checked');
+//     }
+//   });
+// });
